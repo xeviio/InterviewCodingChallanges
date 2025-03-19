4a67b4bdf48c576aaee3b1f9bc2f977ab12f15a6
Kacper Lubach <kacperlubach19@gmail.com>
2025-03-19 16:49:00 +0100

    Update max_number.py

--- CodingChallanges/functions/max_number.py
+++ CodingChallanges/functions/max_number.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 from utils import draw_text, draw_back_button, handle_input
 
+WIDTH=800
+
 def option_max_number(screen, font):
     """Pozwala użytkownikowi wpisać 5 cyfr i zwraca największą z nich."""
     input_active = True
@@ -10,10 +12,10 @@ def option_max_number(screen, font):
 
     while input_active:
         screen.fill((30, 30, 30))
-        draw_text(screen, "Wpisz 5 cyfr:", font, 100)
+        draw_text(screen, "Wpisz 5 cyfr:", font,WIDTH, 100)
 
         # Wyświetlanie wpisanego tekstu
-        draw_text(screen, user_text, font, 150)
+        draw_text(screen, user_text, font,WIDTH, 150)
 
         # Obliczanie największej liczby
         if len(user_text) == 5 and user_text.isdigit():
@@ -24,10 +26,10 @@ def option_max_number(screen, font):
             result_text = "Podaj dokładnie 5 cyfr!"
 
         # Wyświetlanie wyniku
-        draw_text(screen, result_text, font, 200, (0, 255, 0))
+        draw_text(screen, result_text, font,WIDTH, 200, (0, 255, 0))
 
         # Przycisk "Wróć"
-        button_rect = draw_back_button(screen, font)
+        button_rect = draw_back_button(screen, font, WIDTH)
 
         pygame.display.flip()
 
