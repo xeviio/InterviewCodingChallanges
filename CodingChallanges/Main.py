7248b57d848fa7c376e8fa52410ddb81d3b795ca
Kacper Lubach <kacperlubach19@gmail.com>
2025-03-19 16:47:22 +0100

    Update Main.py

--- CodingChallanges/Main.py
+++ CodingChallanges/Main.py
@@ -4,13 +4,13 @@ from functions.hello_world import option_hello_world
 from functions.factorial import option_factorial
 from functions.reverse_string import option_reverse_string
 from functions.max_number import option_max_number
-from utils import draw_text, draw_back_button, handle_input, check_button_click
-
+from functions.sorting_algorithms import option_sorting_algorithms
+from utils import draw_text
 
 pygame.init()
 
 # Ustawienia okna
-WIDTH, HEIGHT = 500, 400
+WIDTH, HEIGHT = 800, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Menu PyInterview")
 font = pygame.font.Font(None, 36)
@@ -19,15 +19,23 @@ menu_options = [
     ("Hello, World!", lambda: option_hello_world(screen, font)),
     ("Oblicz silnię", lambda: option_factorial(screen, font)),
     ("Odwróć string", lambda: option_reverse_string(screen, font)),
-    ("Znajdź największą cyfrę", lambda: option_max_number(screen, font)),  # Dodana opcja
+    ("Znajdź największą cyfrę", lambda: option_max_number(screen, font)),
+    ("Algorytmy sortujące", lambda: option_sorting_algorithms(screen, font)),
     ("Wyjdź", sys.exit)
 ]
 
 def draw_menu():
     """Rysuje menu główne."""
     screen.fill((30, 30, 30))  # Tło ekranu
+
+    # Obliczanie wysokości dla opcji menu na podstawie proporcji ekranu
+    option_height = HEIGHT // (len(menu_options) + 1)  # Dzielimy wysokość na liczbę opcji
+    
     for i, (text, _) in enumerate(menu_options):
-        draw_text(screen, text, font, 50 + i * 50)  # Równomierne rozmieszczenie opcji
+        # Wysokość opcji menu to proporcja wysokości okna
+        y_position = (i + 1) * option_height
+        draw_text(screen, text, font, WIDTH, y_position)  # Równomierne rozmieszczenie opcji
+
     pygame.display.flip()
 
 def main():
@@ -40,10 +48,14 @@ def main():
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
+                option_height = HEIGHT // (len(menu_options) + 1)  # Obliczamy wysokość dla opcji
+
+                # Sprawdzanie kliknięcia na odpowiednią opcję
                 for i, (_, action) in enumerate(menu_options):
-                    if 50 <= y <= 50 + i * 50 + 30:  # Wybór opcji
+                    if (i + 1) * option_height - 30 <= y <= (i + 1) * option_height + 30:
                         action()  # Uruchomienie funkcji powiązanej z opcją
                         break  # Po wykonaniu funkcji wracamy do menu
 
 if __name__ == "__main__":
     main()  # Uruchomienie głównej pętli
+ 
