611fa75f5501bbfe20e2db1c490d2bbfb6a55dff
Kacper Lubach <kacperlubach19@gmail.com>
2025-03-19 16:49:13 +0100

    Update reverse_string.py

--- CodingChallanges/functions/reverse_string.py
+++ CodingChallanges/functions/reverse_string.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 from utils import draw_text, draw_back_button, handle_input, check_button_click
 
+WIDTH=800
+
 def option_reverse_string(screen, font):
     """Wyświetla okno do wprowadzania tekstu i odwracania go."""
     user_text = ""  # Inicjalizacja zmiennej na tekst
@@ -9,17 +11,17 @@ def option_reverse_string(screen, font):
         screen.fill((30, 30, 30))  # Tło ekranu
 
         # Wyświetlanie napisu i prośby o wprowadzenie tekstu
-        draw_text(screen, "Wpisz tekst do odwrócenia:", font, 100)
+        draw_text(screen, "Wpisz tekst do odwrócenia:", font,WIDTH, 100)
 
         # Wyświetlanie wprowadzonego tekstu
-        draw_text(screen, user_text, font, 150)
+        draw_text(screen, user_text, font,WIDTH, 150)
 
         # Odwrócenie tekstu
         reversed_text = user_text[::-1]
-        draw_text(screen, f"{reversed_text}", font, 200,(144, 238, 144))
+        draw_text(screen, f"{reversed_text}", font,WIDTH, 200,(144, 238, 144))
 
         # Rysowanie przycisku "Wróć"
-        button_rect = draw_back_button(screen, font)
+        button_rect = draw_back_button(screen, font, WIDTH)
 
         pygame.display.flip()  # Aktualizacja ekranu
 
